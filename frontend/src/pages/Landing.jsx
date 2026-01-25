834985386db4f6207feadfa5b749989cedc6e1d4
rohit singh jagawat <rohtjagawat45@gmail.com>
2026-01-25 15:15:19 +0530

    new changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -14,7 +14,7 @@ const Index = () => {
             <img
               src="/logo.png"
               alt="Jr. Billionaire Logo"
-              className="w-22 h-22 object-contain"
+              className="w-20 h-20 object-contain"
             />
             <span className="absolute -top-4 -right-4 text-brandRed text-lg">✨</span>
           </div>
