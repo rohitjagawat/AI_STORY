4b39408e479595e7c7e3ae73a0edc1bac6675880
rohit singh jagawat <rohtjagawat45@gmail.com>
2026-01-26 12:15:16 +0530

    webhook changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 
 import storyRoute from "./Routes/story.route.js";
 import shopifyRoutes from "./Routes/shopify.route.js";
@@ -9,17 +8,10 @@ import paymentRoutes from "./Routes/payment.route.js";
 import downloadRoutes from "./Routes/download.route.js";
 import viewRoutes from "./Routes/view.route.js";
 
+
 const app = express();
 
 app.use(cors());
-
-/* 🔥 IMPORTANT: RAW BODY ONLY FOR SHOPIFY WEBHOOK */
-app.use(
-  "/api/shopify/webhook/order-paid",
-  bodyParser.raw({ type: "application/json" })
-);
-
-/* ✅ Normal JSON for rest APIs */
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
@@ -30,10 +22,11 @@ app.use("/images", express.static("images"));
 app.use("/output", express.static("output"));
 
 app.use("/api/story", storyRoute);
-app.use("/api/shopify", shopifyRoutes);   // 👈 keep api prefix
+app.use("/shopify", shopifyRoutes);
 app.use("/api/payment", paymentRoutes);
 app.use("/api/download", downloadRoutes);
 app.use("/api", viewRoutes);
+
 
 const PORT = process.env.PORT || 8080;
 const HOST = "0.0.0.0";
@@ -41,3 +34,4 @@ const HOST = "0.0.0.0";
 app.listen(PORT, HOST, () => {
   console.log(`✅ Backend running on http://${HOST}:${PORT}`);
 });
+
